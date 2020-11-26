2e462ca7c13da40031a9b92ddd5a8679802d3966
Ryan O'Donnell <ryan.jp.odonnell@gmail.com>
2020-11-25 22:00:58 -0800

    Move Sidebar Game cam to Nested Scene

--- sidebar-cam.js
+++ sidebar-cam.js
@@ -11,7 +11,7 @@ module.exports = function() {
       hideItemWithinScene(obs, randomCam, '- Sidebar Cam');
       clearTimeout(randomCamTimeout);
     } else {
-      hideItemWithinScene(obs, 'game', '- Sidebar Cam');
+      hideItemWithinScene(obs, '- Sidebar Game', '- Sidebar Cam');
     }
 
     randomCam = randomElementFromArray(arr);
@@ -22,7 +22,7 @@ module.exports = function() {
 
   hideRandomCam = function (obs) {
     hideItemWithinScene(obs, randomCam, '- Sidebar Cam');
-    showItemWithinScene(obs, 'game', '- Sidebar Cam');
+    showItemWithinScene(obs, '- Sidebar Game', '- Sidebar Cam');
     randomCamActive = false;
   }
 };
