4279fdf838bc75ea30733a2b8a02172ca19bb159
Ryan ODonnell <ryan.jp.odonnell@gmail.com>
2020-12-04 14:32:37 -0800

    Bigger/Smaller; Update cams

--- index.js
+++ index.js
@@ -27,11 +27,8 @@ obs.on('ConnectionOpened', () => {
   ComfyJS.onReward = onRewardHander;
   ComfyJS.Init('gametimetelevision', process.env.OAUTH);
 
-  var babySinclairs = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6'];
-  var urkels = ['u1', 'u2', 'u3', 'u4', 'u5'];
-  var timAllens = ['t1', 't2', 't3', 't4', 't5'];
-  var toomgis = ['z1', 'z2', 'z3'];
-  var bossBabies = ['bb1', 'bb2', 'bb3'];
+  var urkels = ['u1', 'u2', 'u3'];
+  var timAllens = ['t1', 't2', 't3', 't4'];
 
   var commands = [
     '!red',
@@ -44,11 +41,7 @@ obs.on('ConnectionOpened', () => {
     '!yellow',
     '!setcolor',
     '!yabbadabbadoo',
-    '!babysinclaircam',
-    '!bossbabycam',
-    '!grittycam',
     '!urkelcam',
-    '!toomgiscam',
     '!timallencam'
   ];
 
@@ -91,6 +84,68 @@ obs.on('ConnectionOpened', () => {
       setTimeout(hideItemWithinScene, 20000, obs, 'bobl-les', '- Logi Les');
     }
 
+    if (reward === 'BIGGER') {
+      let width = null;
+      let height = null;
+      let scaleX = null;
+      let scaleY = null;
+      let positionX = null;
+      let positionY = null;
+
+      client.say('#gametimetelevision', `!yabbadabbaboo`);
+
+      obs.send('GetSceneItemProperties',{
+        'scene-name': '- Sidebar Big',
+        'item': 'broccoli',
+      }).then(data => {
+        scaleX = (Math.round(data.scale.x * 10) / 10) + 0.1;
+        scaleY = (Math.round(data.scale.y * 10) / 10) + 0.1;
+        width = Math.floor(112 * scaleX);
+        height = Math.floor(112 * scaleY);
+        positionX = 1920 - (width);
+        positionY = 1080 - (height);
+
+        obs.send('SetSceneItemProperties',{
+          'scene-name': '- Sidebar Big',
+          'item': 'broccoli',
+          'scale': { 'x': scaleX, 'y': scaleY },
+          'position': { 'x': positionX, 'y': positionY }
+        });
+      });
+    }
+
+    if (reward === 'smaller') {
+      let width = null;
+      let height = null;
+      let scaleX = null;
+      let scaleY = null;
+      let positionX = null;
+      let positionY = null;
+
+      client.say('#gametimetelevision', `!yabbadabbaboo`);
+
+      obs.send('GetSceneItemProperties',{
+        'scene-name': '- Sidebar Big',
+        'item': 'broccoli',
+      }).then(data => {
+        scaleX = (Math.round(data.scale.x * 10) / 10) - 0.1;
+        scaleY = (Math.round(data.scale.y * 10) / 10) - 0.1;
+        width = Math.floor(112 * scaleX);
+        height = Math.floor(112 * scaleY);
+        positionX = 1920 - (width);
+        positionY = 1080 - (height);
+
+        if (scaleX > 0 && scaleY > 0) {
+          obs.send('SetSceneItemProperties',{
+            'scene-name': '- Sidebar Big',
+            'item': 'broccoli',
+            'scale': { 'x': scaleX, 'y': scaleY },
+            'position': { 'x': positionX, 'y': positionY }
+          });
+        }
+      });
+    }
+
   }
 
   function onMessageHandler (target, context, msg, self) {
@@ -101,30 +156,20 @@ obs.on('ConnectionOpened', () => {
       client.say(target, `The INNOVATIVE commands are: ${commands.join(', ')}`);
     }
 
-    if (commandName === '!babysinclaircam') {
-      showRandomCam(obs, babySinclairs);
-    }
-
     if (commandName === '!urkelcam') {
+      client.say('#gametimetelevision', `!yabbadabbaboo`);
       showRandomCam(obs, urkels);
     }
 
     if (commandName === '!timallencam') {
+      client.say('#gametimetelevision', `!yabbadabbaboo`);
       showRandomCam(obs, timAllens);
     }
 
-    if (commandName === '!grittycam' || commandName === '!toomgiscam') {
-      showRandomCam(obs, toomgis);
-    }
-
-    if (commandName === '!bossbabycam') {
-      showRandomCam(obs, bossBabies);
-    }
-
     if (commandName === '!bobl' || commandName === '!böbl') {
-      client.say(target, `I am way better at böbl than my man ChucklesW69. But you should follow him anyways. He is v handsome and I find it baffling that he does not have a face camera. Think about the VIEWS! Remember when Danny Torrence was maybe molested?`);
-      client.say(target, `https://morphcatgames.itch.io/bobl`);
+      client.say(target, `I am way better at Böbl than my man ChucklesW69. But you should follow him anyways.`);
       client.say(target, `http://twitch.tv/chucklesw73`);
+      client.say(target, `https://morphcatgames.itch.io/bobl`);
     }
 
     if (commandName === '!red' ||
@@ -158,6 +203,13 @@ obs.on('ConnectionOpened', () => {
     console.log(`* Connected to ${addr}:${port}`);
 
     setSidebarColor(obs, 4278190080);
+
+    obs.send('SetSceneItemProperties',{
+      'scene-name': '- Sidebar Big',
+      'item': 'broccoli',
+      'scale': { 'x': 1, 'y': 1 },
+      'position': { 'x': 1808, 'y': 968 }
+    })
   }
 });
 
