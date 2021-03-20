23523b0a6f45598273c4b72ffde020a5695eb9c1
Ryan O'Donnell <ryan.jp.odonnell@gmail.com>
2021-03-19 19:24:09 -0700

    De-affiliate

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ require("./helpers.js")();
 require("./obs-helpers.js")();
 require("./sidebar-cam.js")();
 
-const ComfyJS = require('comfy.js');
 const OBSWebSocket = require('obs-websocket-js');
 const fetch = require('node-fetch');
 const tmi = require('tmi.js');
@@ -25,9 +24,6 @@ obs.on('ConnectionOpened', () => {
   client.on('connected', onConnectedHandler);
   client.connect();
 
-  ComfyJS.onReward = onRewardHander;
-  ComfyJS.Init('gametimetelevision', process.env.OAUTH);
-
   var Map = require('sorted-map');
   var bangers = new Map();
 
@@ -44,112 +40,13 @@ obs.on('ConnectionOpened', () => {
     '!yabbadabbadoo',
     '!discord',
     '!kisses @{USERNAME}',
-    '!leaderboard',
+    '!fartson @{USERNAME}',
     '!tikikoncam'
   ];
-  var badBoys = [];
-
-  function onRewardHander (user, reward, cost, extra) {
-    console.log(`****** ${user} redeemed ${reward} for ${cost} ******`);
-
-    if (reward === 'Bananas on Rod') {
-      client.say('#gametimetelevision', `!yabbadabbaboo`);
-      showItemWithinScene(obs, 'bananas-rod', '- Logi Rod');
-      setTimeout(hideItemWithinScene, 10000, obs, 'bananas-rod', '- Logi Rod');
-    }
-
-    if (reward === 'Bananas on Les') {
-      client.say('#gametimetelevision', `!yabbadabbaboo`);
-      showItemWithinScene(obs, 'bananas-les', '- Logi Les');
-      setTimeout(hideItemWithinScene, 10000, obs, 'bananas-les', '- Logi Les');
-    }
-
-    if (reward === 'Pokeball on Rod') {
-      client.say('#gametimetelevision', `!yabbadabbaboo`);
-      showItemWithinScene(obs, 'pokeball-rod', '- Logi Rod');
-      setTimeout(hideItemWithinScene, 10000, obs, 'pokeball-rod', '- Logi Rod');
-    }
-
-    if (reward === 'Pokeball on Les') {
-      client.say('#gametimetelevision', `!yabbadabbaboo`);
-      showItemWithinScene(obs, 'pokeball-les', '- Logi Les');
-      setTimeout(hideItemWithinScene, 10000, obs, 'pokeball-les', '- Logi Les');
-    }
-
-    if (reward === 'Rod Folds His Laundry') {
-      client.say('#gametimetelevision', `!yabbadabbaboo`);
-    }
-
-    if (reward === 'ROD BIGGER') {
-      client.say('#gametimetelevision', `!yabbadabbaboo`);
-      bigger(obs, '- Logi Rod', 'Logi-Left', 1920, 1080, 640, 360)
-    }
-
-    if (reward === 'rod smaller') {
-      client.say('#gametimetelevision', `!yabbadabbaboo`);
-      smaller(obs, '- Logi Rod', 'Logi-Left', 1920, 1080, 640, 360)
-    }
-
-    if (reward === 'LES BIGGER') {
-      client.say('#gametimetelevision', `!yabbadabbaboo`);
-      bigger(obs, '- Logi Les', 'Logi-Right', 1920, 1080, 640, 360)
-    }
-
-    if (reward === 'les smaller') {
-      client.say('#gametimetelevision', `!yabbadabbaboo`);
-      smaller(obs, '- Logi Les', 'Logi-Right', 1920, 1080, 640, 360)
-    }
-
-    if (reward === 'ZMAC BIGGER') {
-      client.say('#gametimetelevision', `!yabbadabbaboo`);
-      bigger(obs, '- Sidebar Big', 'zmac', 1920, 1080, 112, 112, false)
-      bigger(obs, '- Sidebar', 'zmac', 1920, 1080, 112, 112, false)
-    }
-
-    if (reward === 'zmac smaller') {
-      smaller(obs, '- Sidebar Big', 'zmac', 1920, 1080, 112, 112, false)
-      smaller(obs, '- Sidebar', 'zmac', 1920, 1080, 112, 112, false)
-    }
-  }
 
   function onMessageHandler (target, context, msg, self) {
     console.log(`${context.username} - ${msg}`);
     const commandName = msg.trim().toLowerCase();
-
-    if (badBoys.includes(context.username)) {
-      fetch('http://localhost:4567/enqueue', {
-        method: 'POST',
-        body: JSON.stringify({ user: context.username, comment: escape(msg) }),
-        headers: { 'Content-Type': 'application/json' }
-      }).then(res => {
-        client.deletemessage("gametimetelevision", context.id)
-          .then((data) => {
-            client.say('#gametimetelevision', `!yabbadabbaboo`);
-            console.log('Request complete!');
-          }).catch((err) => {
-            console.log(err);
-          });
-      });
-    }
-
-    if (commandName.startsWith('!badboy') && context.username === 'gametimetelevision') {
-      var badBoy = commandName.split(' ')[1];
-
-      badBoys.push(badBoy);
-      client.say('#gametimetelevision', `${badBoy} added to Bad Boy list`);
-    }
-
-    if (commandName.startsWith('!goodboy') && context.username === 'gametimetelevision') {
-      var goodBoy = commandName.split(' ')[1];
-
-      for (var i = 0; i < badBoys.length; i++) {
-        if ( badBoys[i] === goodBoy) {
-            badBoys.splice(i, 1);
-        }
-      }
-
-      client.say('#gametimetelevision', `${goodBoy} removed from Bad Boy list`);
-    }
 
     if (commandName === '!commands') {
       client.say(target, `The INNOVATIVE commands are: ${commands.join(', ')}`);
@@ -208,6 +105,24 @@ obs.on('ConnectionOpened', () => {
       }
     }
 
+    if (commandName.startsWith('!fartson')) {
+      let username = commandName.split('@')[1];
+
+      if (username !== undefined) {
+        showItemWithinScene(obs, 'fart', '- Sidebar');
+        setTimeout(hideItemWithinScene, 1000, obs, 'fart', '- Sidebar');
+
+        showItemWithinScene(obs, 'fart', '- Sidebar Big');
+        setTimeout(hideItemWithinScene, 1000, obs, 'fart', '- Sidebar Big');
+
+        let bangs = bangers.get(username);
+        bangers.set(username, (bangs || 0) - 1);
+        bangs = bangers.get(username);
+
+        client.say(target, `@${username} has been kissed ${bangs} time(s)`);
+      }
+    }
+
     if (commandName === '!leaderboard') {
       let startLength = bangers.length - 3;
       if (startLength < 0) { startLength = 0 };
@@ -224,31 +139,17 @@ obs.on('ConnectionOpened', () => {
     setSidebarColor(obs, 4278190080);
 
     obs.send('SetSceneItemProperties', {
-      'scene-name': '- Sidebar Big',
-      'item': 'zmac',
-      'scale': { 'x': 1, 'y': 1 },
-      'position': { 'x': 1808, 'y': 968 }
-    })
-
-    obs.send('SetSceneItemProperties', {
-      'scene-name': '- Sidebar',
-      'item': 'zmac',
-      'scale': { 'x': 1, 'y': 1 },
-      'position': { 'x': 1808, 'y': 968 }
-    })
-
-    obs.send('SetSceneItemProperties', {
       'scene-name': '- Logi Rod',
       'item': 'Logi-Left',
-      'scale': { 'x': 5, 'y': 5 },
-      'position': { 'x': -640, 'y': -360 }
+      'scale': { 'x': 3, 'y': 3 },
+      'position': { 'x': -576, 'y': -324 }
     })
 
     obs.send('SetSceneItemProperties', {
       'scene-name': '- Logi Les',
       'item': 'Logi-Right',
-      'scale': { 'x': 5, 'y': 5 },
-      'position': { 'x': -640, 'y': -360 }
+      'scale': { 'x': 3, 'y': 3 },
+      'position': { 'x': -576, 'y': -324 }
     })
   }
 });
